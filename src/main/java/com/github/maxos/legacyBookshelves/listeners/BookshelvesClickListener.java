f9a3a8f84a2f9dd1b228e6f77720911cbfa6f51a
MaxosCraft <pereu1278as@gmail.com>
2026-03-08 21:13:51 +0300

    изменения условий для ивента

--- src/main/java/com/github/maxos/legacyBookshelves/listeners/BookshelvesClickListener.java
+++ src/main/java/com/github/maxos/legacyBookshelves/listeners/BookshelvesClickListener.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -25,9 +26,15 @@ public class BookshelvesClickListener implements Listener {
 
         Block clickedBlock = e.getClickedBlock();
         if (clickedBlock == null || clickedBlock.getType() != Material.BOOKSHELF) return;
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
-        Player p = e.getPlayer();
-        p.openInventory(manager.getInventory(clickedBlock));
+            Player p = e.getPlayer();
+            if (!p.isSneaking()) {
+                p.openInventory(manager.getInventory(clickedBlock));
+                e.setCancelled(true);
+            }
+
+        }
 
         //clickedBlock.getLocation().getBlockX();
         //Bukkit.getWorld("").getBlockAt()
